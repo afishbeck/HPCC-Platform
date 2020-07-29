55428a0552c6a668faa5336f0271558f99cbce02
Anthony Fishbeck <anthony.fishbeck@lexisnexisrisk.com>
2020-07-29 18:55:47 -0400

    HPCC-24546 Support for Hashicorp vaults and kubernetes secrets
    Signed-off-by: Anthony Fishbeck <anthony.fishbeck@lexisnexisrisk.com>

--- common/remote/hooks/azure/azurefile.cpp
+++ common/remote/hooks/azure/azurefile.cpp
@@ -518,7 +518,7 @@ static std::shared_ptr<azure::storage_lite::blob_client> getClient(const char * 
         {
             StringBuffer secretName;
             secretName.append("azure-").append(accountName);
-            getSecret(keyTemp, secretName, "key");
+            getSecretValue(keyTemp, "storage", secretName, "key", true);
             //Trim trailing whitespace/newlines in case the secret has been entered by hand e.g. on bare metal
             size32_t len = keyTemp.length();
             for (;;)
